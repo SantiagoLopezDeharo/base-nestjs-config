bd773ccbc9ac62a2712c6d0103b650db6c2bebb1
santiago lopez de haro <santiagolhgpro@gmail.com>
2025-08-27 11:00:10 -0300

    fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,15 +4,9 @@ import { UserController } from './user.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './entities/user.entity';
 import { AuthModule } from 'src/auth/auth.module';
-import { Cliente } from './entities/client.entity';
-import { Vendedor } from './entities/vendedor.entity';
-import { Admin } from './entities/admin.entity';
 
 @Module({
-  imports: [
-    SequelizeModule.forFeature([User, Cliente, Vendedor, Admin]),
-    AuthModule,
-  ],
+  imports: [SequelizeModule.forFeature([User]), AuthModule],
   controllers: [UserController],
   providers: [UserService],
 })
