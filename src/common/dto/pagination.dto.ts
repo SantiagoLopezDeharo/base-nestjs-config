1dc5f987624143dcd75f415f7bf8453936c9a910
santiago lopez de haro <santiagolhgpro@gmail.com>
2025-08-27 11:03:09 -0300

    fix

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,10 +1,13 @@
+import { Type } from 'class-transformer';
 import { IsNumber, Min } from 'class-validator';
 
 export class PaginationDto {
+  @Type(() => Number)
   @IsNumber()
   @Min(1)
   page: number = 1;
 
+  @Type(() => Number)
   @IsNumber()
   @Min(1)
   size: number = 15;
